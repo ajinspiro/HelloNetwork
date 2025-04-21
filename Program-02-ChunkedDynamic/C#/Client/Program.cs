8774294626cc78da179c973bfd191771984dc9dc
arun <ajarunkumar96@gmail.com>
2025-04-21 07:20:33 +0530

    changes

--- Program-02-ChunkedDynamic/C#/Client/Program.cs
+++ Program-02-ChunkedDynamic/C#/Client/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+
+Console.Write("CLIENT\n\n");
+var (serverIP, serverPort, payloadFilePath) = ParseAndValidateCommandLineArguments();
+await Task.Delay(1000); // Wait till the server starts.
+
+TcpClient tcpClient = new();
+await tcpClient.ConnectAsync(serverIP, serverPort);
+using NetworkStream channel = tcpClient.GetStream();
+using BinaryWriter channelWriter = new(channel); // Using BinaryWriter.Write for simplicity. This is old synchronous API. A modern alternative would be to call the low level Stream(here NetworkStream).ReadAsync directly.
+byte[] imageBytes = GetImageAsBytes();
+channelWriter.Write(imageBytes);
+Console.WriteLine("Sending file IMAGE.jpg complete.");
+tcpClient.Dispose(); // Close the connection
+
+(IPAddress, int, string) ParseAndValidateCommandLineArguments()
+{
+    bool isThere3Args = args.Length == 3;
+
+    if (!isThere3Args)
+    {
+        Console.Error.WriteLine("Incorrect usage.");
+        Console.Error.WriteLine("Usage: >Client.exe 127.0.0.1 11000 C:/folder1/someFile.jpg");
+        Environment.Exit(-1);
+    }
+
+    bool isParsingServerIPSuccess = IPAddress.TryParse(args[0], out IPAddress? ipAddress);
+    bool isPortValid = int.TryParse(args[1], out int port);
+    bool isPayloadPathExists = Path.Exists(args[2]);
+
+    if (!isParsingServerIPSuccess && !isPortValid && !isPayloadPathExists)
+    {
+        Console.Error.WriteLine("Incorrect usage.");
+        Console.Error.WriteLine("Usage: >Client.exe 127.0.0.1 11000 C:/folder1/someFile.jpg");
+        Environment.Exit(-1);
+    }
+
+    return (ipAddress!, port, args[2]);
+}
+
+//static (int metadataLength, string metadata, byte[] fileBytes) PreparePayload()
+//{
+//    Path.GetRelativePath(args)
+//}
+
+static byte[] GetImageAsBytes()
+{
+    string imagePath = "./IMAGE.jpg";
+    using FileStream imageStream = new(imagePath, FileMode.Open, FileAccess.Read);
+    BinaryReader imageReader = new(imageStream);
+    Console.WriteLine(imageStream.Length);
+    if (imageStream.Length > int.MaxValue)
+    {
+        throw new Exception("Can't send files larger than 2GB (int.MaxValue) in this version.");
+    }
+    return imageReader.ReadBytes((int)imageStream.Length);
+}
--- Program-02-ChunkedDynamic/C#/Server/Program.cs
+++ Program-02-ChunkedDynamic/C#/Server/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+
+Console.Write("SERVER\n\n");
+
+IPAddress localhost = IPAddress.Parse("127.0.0.1");
+TcpListener tcpListener = new(localhost, 11000);
+tcpListener.Start();
+while (true)
+{
+    TcpClient connectedClient = await tcpListener.AcceptTcpClientAsync();
+    _ = Task.Run(() => ProcessClient(connectedClient));
+}
+
+static async Task ProcessClient(TcpClient connectedClient)
+{
+    try
+    {
+        using NetworkStream channel = connectedClient.GetStream();
+        using BinaryReader channelReader = new(channel);
+        byte[] imageBytes = channelReader.ReadBytes(90674); // Limitation: server needs to know the size of the transferred file in advance.
+        using FileStream imageStream = new("IMAGE.jpg", FileMode.Create, FileAccess.Write);
+        await imageStream.WriteAsync(imageBytes);
+        Console.WriteLine("File received.");
+        connectedClient.Dispose(); // Close the connection
+    }
+    catch (Exception ex)
+    {
+        Console.Error.WriteLine($"Processing failed. Exception: {ex.GetType().FullName}");
+    }
+}
