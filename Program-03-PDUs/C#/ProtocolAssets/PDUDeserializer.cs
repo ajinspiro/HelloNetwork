aceacb483e8f46d0ad6ade4e55d3d27553d0e48e
Arunkumar Anil Jolly <arunkumar.aniljolly@hrblock.com>
2025-04-24 12:06:44 +0530

    changes

--- Program-03-PDUs/C#/ProtocolAssets/PDUDeserializer.cs
+++ Program-03-PDUs/C#/ProtocolAssets/PDUDeserializer.cs
@@ -2,18 +2,8 @@
 
 namespace ProtocolAssets;
 
-public class PDUConverter
+public class PDUDeserializer
 {
-    public byte[] Serialize(PDU.Connect connect)
-    {
-        int pduSize = 1 + connect.Message.Length; // +1 for type byte
-        byte[] pduBytes = new byte[pduSize];
-        pduBytes[0] = connect.Type;
-        byte[] messageBytes = Encoding.UTF8.GetBytes(connect.Message);
-        Buffer.BlockCopy(messageBytes, 0, pduBytes, 1, messageBytes.Length);
-        return pduBytes;
-    }
-
     public async Task<PDU.Connect> DeserializeConnect(Stream stream)
     {
         byte[] buffer = new byte[8];
@@ -23,13 +13,13 @@ public class PDUConverter
         {
             throw new Exception("Incorrect amount of bytes read.");
         }
-        if (buffer[0] != 1)
+        if (buffer[0] != connectPdu.Type)
         {
             throw new Exception($"Failed to deserialize CONNECT. Type value was {buffer[0]} instead of {connectPdu.Type}");
         }
         bytesRead = await stream.ReadAsync(buffer, 1, 7);
         string connectMessage = Encoding.UTF8.GetString(buffer, 1, 7);
-        if (connectMessage != "CONNECT")
+        if (connectMessage != connectPdu.Message)
         {
             throw new Exception($"Failed to deserialize CONNECT. Message value was {connectMessage} instead of {connectPdu.Message}");
         }
